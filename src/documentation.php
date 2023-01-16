452dca0d3190f830f1448b1d5e85fa0333084018
Julio Cesar Francisquini <julio.francischini@gmail.com>
2023-01-16 09:52:19 -0300

    test

--- src/documentation.php
+++ src/documentation.php
@@ -14,22 +14,4 @@ return [
         'put' => './update.yaml',
         'delete' => './delete.yaml',
     ],
-
-    'http_status_code' => [
-        'ok' => 200,
-        'created' => 201,
-        'no_content' => 204,
-        'unauthorized' => 401,
-        'not_found' => 404,
-        'unprocessable_entity' => 422,
-    ],
-
-    'http_status_code_text' => [
-        'ok' => 'OK',
-        'created' => 'Created',
-        'no_content' => 'No Content',
-        'unauthorized' => 'Unauthorized',
-        'not_found' => 'Not Found',
-        'unprocessable_entity' => 'Unprocessable Content',
-    ]
 ];
