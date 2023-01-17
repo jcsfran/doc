08196fdbfafccbf01843d66ad2a1c15bdd48569d
Julio Cesar Francisquini <julio.francischini@gmail.com>
2023-01-17 14:31:36 -0300

    test

--- src/DocumentationServiceProvider.php
+++ src/DocumentationServiceProvider.php
@@ -19,22 +19,22 @@ class DocumentationServiceProvider extends ServiceProvider
     {
         $configPath = __DIR__ . '/../config/documentation.php';
         $l5SwaggerPatch = __DIR__ . '/../config/l5-swagger.php';
-        $viewsPath = __DIR__ . '/../resources';
+        $resourcesPath = __DIR__ . '/../resources';
+
         $this->publishes([
             $configPath => config_path('documentation.php'),
             $l5SwaggerPatch => config_path('l5-swagger.php'),
-        ], 'config');
+            $resourcesPath . '/views/index.blade.php' => config('l5-swagger.defaults.paths.views') . '/index.blade.php',
 
-        $this->publishes([
-            $viewsPath . '/views/docs.blade.php' => base_path('resources/views/api-docs') . '/docs.blade.php',
-            $viewsPath . '/views/index.blade.php' => config('l5-swagger.defaults.paths.views') . '/index.blade.php',
-            $viewsPath . '/assets' => public_path('docs/assets'),
-            $viewsPath . '/utils' => public_path('docs/utils'),
-            $viewsPath . '/index.yaml' => public_path('docs/index.yaml'),
-            $viewsPath . '/components' => base_path('resources/views/components'),
-            $viewsPath . '/../components' => base_path('app/Views/Components'),
-            $viewsPath . '/responses' => public_path('docs/responses'),
-        ], 'views');
+            $resourcesPath . '/views/docs.blade.php' => base_path('resources/views/api-docs') . '/docs.blade.php',
+            $resourcesPath . '/components' => base_path('resources/views/components'),
+            $resourcesPath . '/../components' => base_path('app/Views/Components'),
+
+            $resourcesPath . '/assets' => public_path('docs/assets'),
+            $resourcesPath . '/utils' => public_path('docs/utils'),
+            $resourcesPath . '/index.yaml' => public_path('docs/index.yaml'),
+            $resourcesPath . '/responses' => public_path('docs/responses'),
+        ]);
 
         $this->commands([
             CreateRouteForDocumentation::class,
